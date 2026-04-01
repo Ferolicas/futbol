f8ebc92a343b8d54d63f24cac6394877f8954bf7
Ferolicas <ferneyolicas@gmail.com>
2026-04-01 18:28:05 +0200

    feat: initial commit

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '2fawn0zp', // Reemplaza esto con el ID de tu proyecto
+    dataset: 'production'
+  }
+})
